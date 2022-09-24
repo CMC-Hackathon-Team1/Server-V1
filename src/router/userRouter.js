f419b3864e166c0c670d32f517d2564fb4d98157
HBSPS <wiyeong79@naver.com>
2022-09-25 05:59:17 +0900

    :adhesive_bandage: 줄 수정
    줄 수정

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -15,8 +15,6 @@ const userRouter = (router) =>{
     
     router.get('/users/:userId/profile',this.userController.getUserProfiles);
 
-    router.get('/users/:userId/profile/statistics',this.userController.getUserProfiles);
-
     // 사용자의 다른 페르소나 가져오기
     router.get('/users/persona', this.userController.changePersona);
 
