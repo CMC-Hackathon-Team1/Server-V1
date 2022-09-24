6d7af19412b4c6ffc027fa4c40617bae105fbaaa
민욱 <alsdnrdl001@gmail.com>
2022-09-25 01:12:37 +0900

    :hammer: update limit 3

--- src/DAO/userDao.js
+++ src/DAO/userDao.js
@@ -5,7 +5,8 @@ class userDAO {
     retrieveUserProfiles = async (conn,userId) => {
         const retrieveUserProfilesQuery = `SELECT profileId,profileImgUrl,profileName FROM Profiles
         RIGHT JOIN Users U on Profiles.userId = U.userId
-        WHERE U.userId=${userId};`;
+        WHERE U.userId=${userId}
+        LIMIT 3;`;
 
         const [profileRow] = await conn.query(retrieveUserProfilesQuery);
 
