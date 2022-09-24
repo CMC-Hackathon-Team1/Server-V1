4bddf7df7aae5e5f1cb55d36e0156d6455c337c7
iamjooon2 <iamjooon2@gmail.com>
2022-09-25 01:20:32 +0900

    :sparkles: 공지 조회 API 개발
    query string으로 페이지네이션 할 수도 있게 함

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,16 @@
 const express  = require("express");
 const router = express.Router();
 
-// const postRouter = require('./post.router');
-// const commentRouter = require('./comment.router');
-// const userRouter = require("./user.router");
-// const messageRouter = require("./message.router");
 const adminRouter = require("./admin.router");
+const feedRouter = require('./feedRouter');
+const userRouter = require('./userRouter');
+const noticeRouter = require('./noticeRouter');
 
 module.exports = () => {
     
-    // postRouter(router);
-    // userRouter(router);
-    // commentRouter(router);
-    // messageRouter(router);
+    feedRouter(router);
+    userRouter(router);
+    noticeRouter(router);
     adminRouter(router);
 
     return router;
