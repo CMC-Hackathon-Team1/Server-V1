cf1896610699c438460848156da7a53a53164a50
민욱 <alsdnrdl001@gmail.com>
2022-09-25 07:39:39 +0900

    :sparkles: update calendar api

--- src/DAO/feedDao.js
+++ src/DAO/feedDao.js
@@ -3,20 +3,24 @@ class feedDAO {
     cosntructor(){}
     
     retrieveMyFeedDate = async (conn,  year,month) => {
-        const retrieveMyFeedDateQuery = `SELECT date_format(createdAt,"%Y-%m-%d") as date FROM Feeds
-        WHERE date_format(createdAt,"%Y-%m")="${year}-${month}";`;
+        const retrieveMyFeedDateQuery = `
+            SELECT date_format(createdAt,"%Y-%m-%d") as date,ImgUrl  FROM Feeds
+            WHERE date_format(createdAt,"%Y-%m")="2022-09"
+            group by date;
+        `;
         
         const [myFeedDateInfo] = await conn.query(retrieveMyFeedDateQuery);
     
 
-        const myFeedDate=new Set();
-        for(let i=0; i<myFeedDateInfo.length;i++){
-            myFeedDate.add(myFeedDateInfo[i].date);
-        }
+        // const myFeedDate=new Set();
+        // for(let i=0; i<myFeedDateInfo.length;i++){
+        //     myFeedDate.add(myFeedDateInfo[i].date);
+        // }
 
-        console.log(myFeedDate);
+        console.log("우하하하");
+        console.log(myFeedDateInfo);
 
-        return myFeedDate;
+        return myFeedDateInfo;
     }
 
     getFeedsByDate = async (conn,  year,month,day,profileId) => {
