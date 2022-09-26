a916142d44c38fc34f2969f4ee32655b1e0458d4
iamjooon2 <iamjooon2@gmail.com>
2022-09-26 12:39:19 +0900

    update README.md

--- src/service/userService.js
+++ src/service/userService.js
@@ -6,17 +6,10 @@ const baseResponse = require('../utility/baseResponseStatus')
 const { errResponse, response } = require('../utility/response');
 
 class userService {
-
     userDAO;
-    // CommentRepository;
-    // PostRepository;
-    // UserRepository;
 
     constructor() {
         this.userDAO = new userDAO();
-        // this.CommentRepository = new CommentRepository();
-        // this.PostRepository = new PostRepository();
-        // this.UserRepository = new UserRepository();
     }
 
     retrieveUserProfiles = async (userId) => {
